114cb68e55accea902ad2449e6096ce3b8f1fd44
qwert50298 <taoxy5@chinaunicom.cn>
2018-11-29 11:13:46 +0800

    精简版唤醒

---
+++ app/src/main/java/com/example/taoxingyu/baiduspeech/ActivityMiniWakeUp.java
@@ -0,0 +1,157 @@
+package com.example.taoxingyu.baiduspeech;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by fujiayi on 2017/8/15.
+ */
+
+public class ActivityMiniWakeUp extends AppCompatActivity implements EventListener {
+    protected TextView txtLog;
+    protected TextView txtResult;
+    protected Button btn;
+    protected Button stopBtn;
+    private static String DESC_TEXT = "精简版唤醒，带有SDK唤醒运行的最少代码，仅仅展示如何调用，\n" +
+            "也可以用来反馈测试SDK输入参数及输出回调。\n" +
+            "本示例需要自行根据文档填写参数，可以使用之前唤醒示例中的日志中的参数。\n" +
+            "需要完整版请参见之前的唤醒示例。\n\n" +
+            "唤醒词是纯离线功能，需要获取正式授权文件（与离线命令词的正式授权文件是同一个）。 第一次联网使用唤醒词功能自动获取正式授权文件。之后可以断网测试\n" +
+            "请说“小度小度”\n\n";
+
+    private EventManager wakeup;
+
+    private boolean logTime = true;
+
+    /**
+     * 测试参数填在这里
+     */
+    private void start() {
+        txtLog.setText("");
+        Map<String, Object> params = new TreeMap<String, Object>();
+
+        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
+        params.put(SpeechConstant.WP_WORDS_FILE, "assets:///WakeUp.bin");
+        // "assets:///WakeUp.bin" 表示WakeUp.bin文件定义在assets目录下
+
+        String json = null; // 这里可以替换成你需要测试的json
+        json = new JSONObject(params).toString();
+        wakeup.send(SpeechConstant.WAKEUP_START, json, null, 0, 0);
+        printLog("输入参数：" + json);
+    }
+
+    private void stop() {
+        wakeup.send(SpeechConstant.WAKEUP_STOP, null, null, 0, 0); //
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.common_mini);
+        initView();
+        initPermission();
+        wakeup = EventManagerFactory.create(this, "wp");
+        wakeup.registerListener(this); //  EventListener 中 onEvent方法
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stop();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        wakeup.send(SpeechConstant.WAKEUP_STOP, "{}", null, 0, 0);
+    }
+
+    //   EventListener  回调方法
+    @Override
+    public void onEvent(String name, String params, byte[] data, int offset, int length) {
+        String logTxt = "name: " + name;
+        if (params != null && !params.isEmpty()) {
+            logTxt += " ;params :" + params;
+        } else if (data != null) {
+            logTxt += " ;data length=" + data.length;
+        }
+        printLog(logTxt);
+    }
+
+    private void printLog(String text) {
+        if (logTime) {
+            text += "  ;time=" + System.currentTimeMillis();
+        }
+        text += "\n";
+        Log.i(getClass().getName(), text);
+        txtLog.append(text + "\n");
+    }
+
+
+    private void initView() {
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        txtLog = (TextView) findViewById(R.id.txtLog);
+        btn = (Button) findViewById(R.id.btn);
+        stopBtn = (Button) findViewById(R.id.btn_stop);
+        txtLog.setText(DESC_TEXT + "\n");
+    }
+
+    /**
+     * android 6.0 以上需要动态申请权限
+     */
+    private void initPermission() {
+        String permissions[] = {Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        ArrayList<String> toApplyList = new ArrayList<String>();
+
+        for (String perm : permissions) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, perm)) {
+                toApplyList.add(perm);
+                // 进入到这里代表没有权限.
+
+            }
+        }
+        String tmpList[] = new String[toApplyList.size()];
+        if (!toApplyList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, toApplyList.toArray(tmpList), 123);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // 此处为android 6.0以上动态授权的回调，用户自行实现。
+    }
+
+}
